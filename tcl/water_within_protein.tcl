b3110924bc705e78324251e4880d9b4020ccdcda
Eva Agnes Bertalan <bertalae93@physik.fu-berlin.de>
2019-06-14 18:02:47 +0200

    change parameter to filelist

--- tcl/water_within_protein.tcl
+++ tcl/water_within_protein.tcl
@@ -1,4 +1,4 @@
-#execute: vmd -dispdev text -e water_within_protein.tcl -args FOLDER_NAME PARAMETERS> water_count.out
+#execute: vmd -dispdev text -e water_within_protein.tcl -args CODE FILELIST PARAMETERS> water_count.out
 # e.g: FOLDER_NAME = 6b73B
 # e.g: PARAMETERS = 11 9 15 13 12 18 5
 
@@ -9,22 +9,26 @@ mol off top
 set first_frame 0
 set last_frame -1
 
-set filelist [glob ../../$code/results/namd/step7.*_production.dcd-pbc.dcd]
+set files [lindex $argv 1]
+set filelist [split [lindex $files 0]]
 set nf [llength $filelist]
 
-set x [lindex $argv 1]
-set y [lindex $argv 2]
-set z [lindex $argv 3]
-set _x [lindex $argv 4]
-set _y [lindex $argv 5]
-set _z [lindex $argv 6]
-set distance [lindex $argv 7]
+set x [lindex $argv 2]
+set y [lindex $argv 3]
+set z [lindex $argv 4]
+set _x [lindex $argv 5]
+set _y [lindex $argv 6]
+set _z [lindex $argv 7]
+set distance [lindex $argv 8]
 
 set selection "(water within $distance of protein) and (x<$x and y<$y and z<$z and -x<$_x and -y<$_y and -z<$_z) and oxygen"
 
-for { set i 1 } { $i <= $nf } { incr i } {
-	set crnt_file [glob ../../$code/results/namd/step7.${i}_production.dcd-pbc.dcd]
-
+for { set i 0 } { $i <= $nf } { incr i } {
+    set file_name [lindex $filelist $i]
+    puts file_name
+    set crnt_file [glob ../$file_name-pbc.dcd]
+    puts crnt_file
+    
 	animate read dcd $crnt_file beg 0 end -1 waitfor all
 
 	set num_steps [molinfo top get numframes]
